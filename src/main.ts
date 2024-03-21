da77aadc7825eeec48bb5a9551a2d2ca2f9e3b16
Sergio <108098770+sergiobraiscompo@users.noreply.github.com>
2024-03-21 19:47:17 +0100

    Actualizar main.ts
    Añadido botón personalizado

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 let turno = 0;
 
-
 // Función mostrar número
 const mostrar_numero = () => {
     const numero_turno_element = document.getElementById("numero_turno_element");
@@ -33,6 +32,10 @@ const handle_click = (boton: string) => {
                 break;
             }
         }
+case "personalizado" {
+turno = turnoInput
+}
+
         case "reset": {
                 turno = 0, mostrar_numero;
                 break;
@@ -48,6 +51,13 @@ boton_anterior?.addEventListener("click", () => handle_click("anterior"));
 // Botón número siguiente
 const boton_siguiente = document.getElementById("siguiente");
 boton_siguiente?.addEventListener("click", () => handle_click("siguiente"));
+
+// Recibe número personalizado
+const turno_input = document.getElementByID("turno_personalizado");
+
+// Botón personalizado
+const boton_personalizado = document.getElementById("personalizado");
+boton_personalizado?.addEventListener("click", () => handle_click("personalizado", turno_input));
 
 // Botón reset
 const boton_reset = document.getElementById("reset");
